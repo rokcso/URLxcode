6459e69546091b961b4ce8441e4b71204b6fbf8d
waringhu <waringhu@163.com>
2023-08-07 12:28:45 +0800

    adjust button layout, add encode mode

--- static/js/index.js
+++ static/js/index.js
@@ -6,160 +6,167 @@ document.getElementById("btn-clear").addEventListener("click", clear);
 document.getElementById("btn-swap").addEventListener("click", swap);
 document.getElementById("btn-parse").addEventListener("click", parseAllURL);
 document.getElementById("btn-copy").addEventListener("click", copy);
-document.getElementById("input-text").addEventListener("input", clearError)
+document.getElementById("input-text").addEventListener("input", clearError);
+
 let errorMsgDiv;
 
-
 function decode() {
-    let input = document.getElementById("input-text").value;
-    clearError();
-    
-    if (!isValidDecodeInput(input)) {
-        showError(decodeErrorMsg);
-        return;
-    }
+  let input = document.getElementById("input-text").value;
+  clearError();
 
-    let output = decodeURIComponent(input);
+  if (!isValidDecodeInput(input)) {
+    showError(decodeErrorMsg);
+    return;
+  }
 
-    showOutput(output);
+  let output = decodeURIComponent(input);
+
+  showOutput(output);
 }
 
 function encode() {
-    let input = document.getElementById("input-text").value;
-    clearError();
+  const special = document.getElementById("encode-special");
+  const all = document.getElementById("encode-all");
 
-    if (!isValidEncodeInput(input)) {
-        showError(encodeErrorMsg);
-        return;
-    }
+  let input = document.getElementById("input-text").value;
+  clearError();
 
-    let output = encodeURIComponent(input);
+  if (!isValidEncodeInput(input)) {
+    showError(encodeErrorMsg);
+    return;
+  }
 
-    showOutput(output);
+  let output;
+  if (special.checked) {
+    output = encodeURI(input);
+  } else if (all.checked) {
+    output = encodeURIComponent(input);
+  }
+
+  showOutput(output);
 }
 
 function clear() {
-    const inputBox = document.getElementById("input-text");
-    const outputBox = document.getElementById("output-text");
+  const inputBox = document.getElementById("input-text");
+  const outputBox = document.getElementById("output-text");
 
-    inputBox.value = "";
-    outputBox.value = "";
+  inputBox.value = "";
+  outputBox.value = "";
 
-    clearError();
+  clearError();
 }
 
 function swap() {
-    const inputBox = document.getElementById("input-text");
-    const outputBox = document.getElementById("output-text");
+  const inputBox = document.getElementById("input-text");
+  const outputBox = document.getElementById("output-text");
 
-    const inputValue = inputBox.value;
-    inputBox.value = outputBox.value;
-    outputBox.value = inputValue;
+  const inputValue = inputBox.value;
+  inputBox.value = outputBox.value;
+  outputBox.value = inputValue;
 }
 
 function parseAllURL() {
-    const inputBox = document.getElementById('input-text');
-    const outputBox = document.getElementById('output-text');
-    let inputURL = inputBox.value;
-    let outputURL = outputBox.value;
-    let inputParse = parse(inputURL);
-    let ouputParse = parse(outputURL);
+  const inputBox = document.getElementById("input-text");
+  const outputBox = document.getElementById("output-text");
+  let inputURL = inputBox.value;
+  let outputURL = outputBox.value;
+  let inputParse = parse(inputURL);
+  let ouputParse = parse(outputURL);
 
-    if (inputParse !== 0) {
-        inputBox.value = inputParse;
-    }
-    if (ouputParse !== 0) {
-        outputBox.value = ouputParse;
-    }
+  if (inputParse !== 0) {
+    inputBox.value = inputParse;
+  }
+  if (ouputParse !== 0) {
+    outputBox.value = ouputParse;
+  }
 }
 
 function parse(url) {
-    if (!url) {
-        console.log("Parse Error: No input.");
-        return 0;
-    }
-    let urlTrim = url.trim();
-    if (urlTrim.includes("\n")) {
-        console.log("Parse Error: Has been parsed.");
-        return 0;
-    }
-    const urlPattern = /^(\w+):\/\//;
-    if (!urlPattern.test(url)) {
-        console.log("Parse Error: Not in URL format.");
-        return 0;
-    }
-    const protocol = /^(\w+):\/\//.exec(url)[1];
-    const hostname = /^(\w+):\/\/([^/]+)/.exec(url)[2];
-    const pathname = /^(\w+):\/\/[^/]+(\/*[^?]*)/.exec(url)[2];
-    const params = /[?](.*)$/.exec(url)[1];
-    let result = `${protocol}://${hostname}${pathname}\n`;
-    params.split('&').forEach(param => {
-        result += `${param}\n`;
-      });
-    return result;
+  if (!url) {
+    console.log("Parse Error: No input.");
+    return 0;
+  }
+  let urlTrim = url.trim();
+  if (urlTrim.includes("\n")) {
+    console.log("Parse Error: Has been parsed.");
+    return 0;
+  }
+  const urlPattern = /^(\w+):\/\//;
+  if (!urlPattern.test(url)) {
+    console.log("Parse Error: Not in URL format.");
+    return 0;
+  }
+  const protocol = /^(\w+):\/\//.exec(url)[1];
+  const hostname = /^(\w+):\/\/([^/]+)/.exec(url)[2];
+  const pathname = /^(\w+):\/\/[^/]+(\/*[^?]*)/.exec(url)[2];
+  const params = /[?](.*)$/.exec(url)[1];
+  let result = `${protocol}://${hostname}${pathname}\n`;
+  params.split("&").forEach((param) => {
+    result += `${param}\n`;
+  });
+  return result;
 }
 
 function copy() {
-    const outputBox = document.getElementById("output-text");
-    const copyBtn = document.getElementById("btn-copy");
-    const outputValue = outputBox.value;
+  const outputBox = document.getElementById("output-text");
+  const copyBtn = document.getElementById("btn-copy");
+  const outputValue = outputBox.value;
 
-    navigator.clipboard.writeText(outputValue);
-    copyBtn.textContent = copyBtnTextAfter;
-    copyBtn.style.background = "#40c060";
+  navigator.clipboard.writeText(outputValue);
+  copyBtn.textContent = copyBtnTextAfter;
+  copyBtn.style.background = "#40c060";
 
-    setTimeout(() => {
-        copyBtn.textContent = copyBtnTextBefore;
-        copyBtn.style.background = "#4c9aff";
-    }, 250);
+  setTimeout(() => {
+    copyBtn.textContent = copyBtnTextBefore;
+    copyBtn.style.background = "#4c9aff";
+  }, 250);
 }
 
 function isValidDecodeInput(input) {
-    if (!input) {
-        return false;
-    }
+  if (!input) {
+    return false;
+  }
 
-    try {
-        decodeURIComponent(input);
-        return true;
-    } catch {
-        return false;
-    }
+  try {
+    decodeURIComponent(input);
+    return true;
+  } catch {
+    return false;
+  }
 }
 
 function isValidEncodeInput(input) {
-    if (!input) {
-        return false;
-    }
+  if (!input) {
+    return false;
+  }
 
-    try {
-        encodeURIComponent(input);
-        return true;
-    } catch {
-        return false;
-    }
+  try {
+    encodeURIComponent(input);
+    return true;
+  } catch {
+    return false;
+  }
 }
 
 function showError(msg) {
-    const input = document.getElementById("input-text");
-    input.style.border = "1px solid red";
+  const input = document.getElementById("input-text");
+  input.style.border = "1px solid red";
 
-    errorMsgDiv = document.createElement("div");
-    errorMsgDiv.textContent = msg;
-    errorMsgDiv.style.color = "red";
-    input.parentElement.insertBefore(errorMsgDiv, input.nextSibling);
+  errorMsgDiv = document.createElement("div");
+  errorMsgDiv.textContent = msg;
+  errorMsgDiv.style.color = "red";
+  input.parentElement.insertBefore(errorMsgDiv, input.nextSibling);
 }
 
 function clearError() {
-    const input = document.getElementById("input-text");
-    if (errorMsgDiv) {
-        errorMsgDiv.remove();
-    }
-    input.style.border = "1px solid #ccc";
+  const input = document.getElementById("input-text");
+  if (errorMsgDiv) {
+    errorMsgDiv.remove();
+  }
+  input.style.border = "1px solid #ccc";
 }
 
 function showOutput(output) {
-    const outputInput = document.getElementById('output-text');
-    outputInput.value = output;
+  const outputInput = document.getElementById("output-text");
+  outputInput.value = output;
 }
-
